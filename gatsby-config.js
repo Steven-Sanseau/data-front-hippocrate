45718e570d0fa972c730eeba50ef012a0c57ff45
Steven Sanséau <steste.etsets@gmail.com>
2018-05-18 20:01:02 +0200

    fix heroku deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,15 @@
+// gatsby-config.js
+
+let activeEnv = process.env.ACTIVE_ENV
+
+if (!activeEnv) {
+  activeEnv = 'development'
+}
+
+require('dotenv').config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Hippocratic Oath for Data Scientist`,
