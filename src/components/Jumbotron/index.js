7ee6d7bd41fa7dc78133a826478ad46ec598889b
Steven Sanséau <steste.etsets@gmail.com>
2018-05-30 15:13:50 +0200

    add animation on glasses

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,55 +1,71 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Image, Flex, Box, Text } from 'rebass'
+import anime from 'animejs'
 
-const Jumbotron = () => (
-  <Flex mx={-2} justifyContent="center" wrap>
-    <Box width={[1, 1 / 2]} px={2}>
-      <Text
-        color="dark_grey"
-        textAlign="left"
-        fontSize={14}
-        fontWeight={300}
-        style={{ opacity: 0.3 }}
-        ml={['0', '10%', '40%']}
-      >
-        Data for Good propose un
-      </Text>
-      <Text
-        textAlign="right"
-        fontSize={45}
-        fontWeight={700}
-        color="dark_grey"
-        lineHeight="1.5em"
-      >
-        Serment d'Hippocrate<br />pour Data Scientist
-      </Text>
-      <Text
-        color="dark_grey"
-        textAlign="right"
-        fontSize="14"
-        mt={9}
-        lineHeight="45px"
-        fontWeight={300}
-        style={{ opacity: 0.3 }}
-      >
-        …ou pour toute personne travaillant avec la donnée
-      </Text>
-    </Box>
-    <Box width={[1, 1 / 2]} px={2}>
-      <Image
-        ml="2em"
-        style={{ opacity: 0.2 }}
-        width={['100%', '25%']}
-        src="/static/hippocrate.png"
-      />
-      <Image
-        width={['100%', '14.5%']}
-        mt="-17.5%"
-        ml="8.1%"
-        src="/static/glasses.png"
-      />
-    </Box>
-  </Flex>
-)
+export default class Jumbotron extends Component {
+  componentDidMount = () => {
+    const cssProperties = anime({
+      targets: '#glasses',
+      // marginTop: '-17.5%',
+      translateY: 104,
+      opacity: '1',
+      easing: 'linear',
+      duration: 1500,
+    })
+  }
 
-export default Jumbotron
+  render() {
+    return (
+      <Flex mx={-2} justifyContent="center" wrap>
+        <Box width={[1, 1 / 2]} px={2}>
+          <Text
+            color="dark_grey"
+            textAlign="left"
+            fontSize={14}
+            fontWeight={300}
+            style={{ opacity: 0.3 }}
+            ml={['0', '10%', '40%']}
+          >
+            Data for Good propose un
+          </Text>
+          <Text
+            textAlign="right"
+            fontSize={45}
+            fontWeight={700}
+            color="dark_grey"
+            lineHeight="1.5em"
+          >
+            Serment d'Hippocrate<br />pour Data Scientist
+          </Text>
+          <Text
+            color="dark_grey"
+            textAlign="right"
+            fontSize="14"
+            mt={9}
+            lineHeight="45px"
+            fontWeight={300}
+            style={{ opacity: 0.3 }}
+          >
+            …ou pour toute personne travaillant avec la donnée
+          </Text>
+        </Box>
+        <Box width={[1, 1 / 2]} px={2}>
+          <Image
+            ml="2em"
+            style={{ opacity: 0.2 }}
+            width={['100%', '25%']}
+            src="/static/hippocrate.png"
+          />
+          <Image
+            width={['100%', '14.5%']}
+            style={{ opacity: 0 }}
+            mt="-30%"
+            ml="8.1%"
+            src="/static/glasses.png"
+            id="glasses"
+          />
+        </Box>
+      </Flex>
+    )
+  }
+}
